febb1b85f41c45168241b0c8688ff223a2836c87
Timilehin Emmanuel <Harriemannie@gmail.com>
2021-04-14 14:01:21 +0100

    create dashboard controller, style dashboard and property pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get "/dashboard" => 'dashboard#index', as: :dashboard
+  get 'dashboard/properties'
+  get 'dashboard/reports'
+  devise_for :accounts
+  resources :properties
   root to: 'public#main'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
